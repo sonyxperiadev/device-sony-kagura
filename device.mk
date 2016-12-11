d2cde07687ab6e551400d3b372d4e236d1c72fef
Adrian DC <radian.dc@gmail.com>
2016-12-11 11:45:37 +0100

    kagura: config: Use unique variables for device paths
     * Avoid hardcoding the paths by using common
        variables declared once for path accesses
    
    Change-Id: I85bf3f9ec81b585daa0330149ebb873ada5335bf
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- device.mk
+++ device.mk
@@ -12,18 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Device path
+DEVICE_PATH := device/sony/kagura
+
 DEVICE_PACKAGE_OVERLAYS += \
-    device/sony/kagura/overlay
+    $(DEVICE_PATH)/overlay
 
 PRODUCT_COPY_FILES := \
-    device/sony/kagura/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    device/sony/kagura/rootdir/system/etc/BCM4359C0.hcd:system/etc/firmware/BCM43xx.hcd \
-    device/sony/kagura/rootdir/system/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
-    device/sony/kagura/rootdir/system/etc/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
-    device/sony/kagura/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
-    device/sony/kagura/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/sony/kagura/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-    device/sony/kagura/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(DEVICE_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    $(DEVICE_PATH)/rootdir/system/etc/BCM4359C0.hcd:system/etc/firmware/BCM43xx.hcd \
+    $(DEVICE_PATH)/rootdir/system/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    $(DEVICE_PATH)/rootdir/system/etc/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
+    $(DEVICE_PATH)/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
+    $(DEVICE_PATH)/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    $(DEVICE_PATH)/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    $(DEVICE_PATH)/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Device Specific Permissions
 PRODUCT_COPY_FILES += \
