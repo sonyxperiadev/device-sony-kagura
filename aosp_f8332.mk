13ad19b4814bf837dde1bb9fc30eb84f00ed47eb
Alin Jerpelea <alin.jerpelea@sony.com>
2018-08-22 12:43:12 +0200

    reorder properties for DS devices
    The ro.telephony.default_network was not properly set without the
    reorder
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- aosp_f8332.mk
+++ aosp_f8332.mk
@@ -12,14 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, device/sony/kagura/aosp_f8331.mk)
-
 # DualSim
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.multisim.config=dsds \
     persist.radio.multisim.config=dsds \
     ro.telephony.default_network=9,1
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/kagura/aosp_f8331.mk)
 
 PRODUCT_NAME := aosp_f8332
 PRODUCT_DEVICE := kagura
